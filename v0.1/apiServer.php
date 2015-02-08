55eae3bb4d0b67cae527f28be50937005bf76f19
dev-101 <dev-101@users.noreply.github.com>
2015-02-08 20:34:51 +0100

    Update apiServer.php

--- v0.1/apiServer.php
+++ v0.1/apiServer.php
@@ -1,15 +1,9 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * Description of apiServer
  *
- * This api for Osclass
+ * This is API for Osclass
  * 
  */
 class apiServer {
